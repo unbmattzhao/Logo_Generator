8f9ab0b849206ddf23522c4afe3ae277955fa2d8
Matt Zhao <46049501+JediRebel@users.noreply.github.com>
2023-03-21 22:23:23 -0300

    folders and files initialized

---
+++ index.js
@@ -0,0 +1,14 @@
+import fs from "fs";
+import inquirer from "inquirer";
+// import from;
+// import from;
+// import from;
+
+inquirer
+  .prompt([{}, {}, {}, {}])
+  .then((answers) => {})
+  .catch((error) => {
+    if (error.isTtyError) {
+    } else {
+    }
+  });
