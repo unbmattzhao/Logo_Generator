0e0f1bc669f565e89066128f8c5fb0eb29286c7f
Matt Zhao <mattzhao@MP.ht.home>
2023-03-26 22:56:17 -0300

    shapes and main functions work

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-import fs from "fs";
-import inquirer from "inquirer";
-import colorName from "color-name";
-import("./shapes.js");
-// import from;
-// import from;
+var fs = require("fs");
+var inquirer = require ("inquirer");
+var colorName = require ("color-name");
+var { Circle, Triangle, Square } = require("./lib/shapes.js");
 
 function colorValidate(input) {
+  var color = input.toLowerCase();
+
   // check if input is a valid color keyword
-  if (colorName[input.toLowerCase()]) {
+  if (colorName[color]) {
     return true;
   }
 
-  // check if input is a valid hexadecimal color code
-  if (/^#?([0-9a-fA-F]{3}){1,2}$/.test(input)) {
-    return true;
+  if (!color.startsWith("#")) {
+    color = `#${color}`;
+    if (/^#?([0-9a-fA-F]{3}){1,2}$/.test(color)) {
+      return true;
+    }
   }
+  // check if input is a valid hexadecimal color code with or without the "#" character
+
 
   return "Please enter a valid color keyword or hexadecimal color code";
 }
 
+
 inquirer
   .prompt([
     {
-      type: " input",
+      type: "input",
       message: "Please select up to three characters as your logo text",
       name: "logoText",
       validate: function (input) {
-        return input.length < 4
+        return input.length <= 3
           ? true
-          : "Please enter less than three characters";
+          : "Please enter up to three characters";
       },
     },
     {
@@ -47,18 +52,59 @@ inquirer
     {
       type: "input",
       message:
-        "Please enter a color keyword OR a hexadecimal code for the shape your selected",
+        "Please enter a color keyword OR a hexadecimal code for the shape you selected",
       name: "shapeColor",
       validate: colorValidate,
     },
   ])
-  .then((answers) => {
-    answers.jason();
+  .then(async (answers) => {
+    const { shape, logoText, textColor, shapeColor } = answers;
+
+    // Create a new shape object based on user input
+    let logoShape;
+    switch (shape) {
+      case "Circle":
+        logoShape = new Circle();
+        break;
+      case "Triangle":
+        logoShape = new Triangle();
+        break;
+      case "Square":
+        logoShape = new Square();
+        break;
+      default:
+        console.log("Invalid shape selected");
+        return;
+    }
+
+    // Set the color of the shape object based on user input
+
+    // Create a new text object based on user input
+    const logoTextObj = {
+      text: logoText,
+      color: textColor
+    }
+    logoShape.setColor(shapeColor);
+
+    // Generate the logo SVG and save it to a file
+    const logoSVG = `<svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg">${logoShape.render()}<text x="150" y="120" font-size="65" fill="${logoTextObj.color}" text-anchor="middle" alignment-baseline="central">${logoTextObj.text}</text></svg>`;
+
+
+    const logoFilename = `${logoText}.svg`;
+    try {
+      await fs.writeFile(logoFilename, logoSVG, function (err) {
+        if (err) throw err;
+        console.log(`Logo generated and saved to file: ${logoFilename}`);
+      });
+    } catch (error) {
+      console.log(`Error writing file: ${error.message}`);
+    }
+    
   })
   .catch((error) => {
     if (error.isTtyError) {
+      console.log("Prompt cannot be rendered in the current environment");
     } else {
+      console.log("Error:", error);
     }
   });
-
-function generateLogo(shape, logoText, textColor, logoColor) {}
